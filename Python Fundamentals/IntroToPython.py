f4ee94880ee2f50239d7e3bf550b5264fa940996
Scott Hadzik <scotthadzik@weber.edu>
2019-01-08 14:04:30 -0700

    Added information for python

---
+++ Python Fundamentals/IntroToPython.py
@@ -0,0 +1,15 @@
+# https://linuxconfig.org/python-files-and-the-interpreter
+# This is a Comment
+#   Python is an interpreted language
+#       --> It is compiled everytime that it runs
+    #   Advatages
+    #       --> Easier to Debug
+    #               Fails imediately when run
+    #               Comiled laguages may compile but not run
+    #       --> Very portable 
+    #               all you need is the interpreter to run python
+    #   Disadvantage
+    #       --> Slower. Inefficient to compile code everytime you run it
+# Can be run in a real time interpreter or written in a file and run in the terminal
+
+# Spacing matters
